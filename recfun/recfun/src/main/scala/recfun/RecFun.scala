96a9397015ad6499bcf6489fbb9c12f06dbed4f4
Victor Da Camino <victordacamino@gmail.com>
2022-08-21 22:17:33 +0200

    First week assignments

--- recfun/recfun/src/main/scala/recfun/RecFun.scala
+++ recfun/recfun/src/main/scala/recfun/RecFun.scala
@@ -12,14 +12,29 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {  
+    if (c == 0 || c == r) 1  
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)  
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceRec(acc: Int, chars: List[Char]): Int = {
+      if (chars.isEmpty || acc < 0)  acc
+      else if (chars.head == '(') balanceRec(acc + 1, chars.tail)
+      else if (chars.head == ')') balanceRec(acc - 1, chars.tail)
+      else balanceRec(acc, chars.tail)
+    }
+    balanceRec(0, chars) == 0
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {  
+    if(money==0 ) 1  
+    else if(coins.isEmpty || money < 0) 0  
+    else countChange(money, coins.tail) + countChange(money-coins.head, coins)  
+  }
